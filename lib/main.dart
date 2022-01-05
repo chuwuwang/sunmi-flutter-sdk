f5cb0802da8275db48f1a7ff639ddc2c6fbfab6c
zhou <chuwuwang@126.com>
2022-01-05 16:54:11 +0800

    feat：增加首页逻辑

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sunmi_flutter_sdk/theme/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,52 +12,62 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter Sdk'),
+          title: const Text('Flutter Sdk'),
         ),
-        body: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(
-                    flex: 1,
-                    child: MaterialButton(
-                      height: 112,
-                      onPressed: () {},
-                      child: const Text('Basic'),
-                      textColor: const Color(0xFFFFFFFF),
-                      color: const Color(0xFF66CC33),
-                    )),
-                Expanded(
-                    flex: 1,
-                    child: MaterialButton(
-                      onPressed: () {},
-                      child: Text('ReadCard'),
-                    )),
-              ],
-            ),
-            Row(
-              children: [
-                MaterialButton(
-                  onPressed: () {},
-                  child: Text('Security'),
-                ),
-                MaterialButton(
-                  onPressed: () {},
-                  child: Text('PinPad'),
-                )
-              ],
-            )
-          ],
-        ),
+        body: _mainView(),
       ),
     );
   }
+}
+
+_mainView() {
+  return SingleChildScrollView(
+    child: Container(
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        children: [
+          Row(
+              children:[
+                _tabItem('Basic', ColorHelper.basic, () {}),
+                _tabItem('Print', ColorHelper.print, () {}),
+              ]
+          ),
+          Row(
+              children:[
+                _tabItem('Scan', ColorHelper.security, () {}),
+                _tabItem('EMV', ColorHelper.emv, () {}),
+              ]
+          )
+        ],
+      ),
+    ),
+  );
+}
+
+_tabItem(String text, Color color, VoidCallback onPressed) {
+  return Expanded(
+      flex: 1,
+      child: Container(
+        height: 112,
+        margin: const EdgeInsets.fromLTRB(4, 4, 4, 4),
+        decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(
+                Radius.circular(8)
+            )
+        ),
+        child: MaterialButton(
+          onPressed: onPressed,
+          child: Text(text,
+              style: const TextStyle(color: Colors.white, fontSize: 18)
+          ),
+          color: color,
+        ),
+      )
+  );
 }
 
 class MyHomePage extends StatefulWidget {
---
+++ lib/theme/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorHelper {
+  static const Color emv = Color(0XFFFF6600);
+  static const Color other = Color(0XFF607B8B);
+  static const Color basic = Color(0XFF66CC33);
+  static const Color print = Color(0XFF3399FF);
+  static const Color security = Color(0XFFFFCC00);
+}
