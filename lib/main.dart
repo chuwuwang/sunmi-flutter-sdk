a1e9a5169a52d2a74552ed2ccb48ee6e16f6d701
zhou <nishenzhou@sunmi.com>
2022-02-08 19:03:44 +0800

    feat: 增加路由跳转功能

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:sun_mi_flutter_sdk/theme/colors.dart';
+import 'package:flutter/services.dart';
+import 'package:sun_mi_flutter_sdk/pages/HomeScreen.dart';
 
 void main() {
+  if (Platform.isAndroid) {
+    SystemChrome.setSystemUIOverlayStyle(
+        const SystemUiOverlayStyle(statusBarColor: Colors.transparent)
+    );
+  }
   var app = const MyApp();
-  runApp(app);
+    runApp(app);
 }
 
 class MyApp extends StatelessWidget {
@@ -15,59 +23,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('S U N M I Flutter Sdk'),
-        ),
-        body: _mainView(context),
-      ),
+      home: const HomeScreen()
     );
   }
 
-}
-
-_mainView(BuildContext context) {
-  return SingleChildScrollView(
-    child: Container(
-      padding: const EdgeInsets.all(8),
-      child: Column(
-        children: [
-          Row(
-              children:[
-                _tabItem('Basic', ColorHelper.basic, () {}),
-                _tabItem('Print', ColorHelper.print, () {}),
-              ]
-          ),
-          Row(
-              children:[
-                _tabItem('Scan', ColorHelper.security, () {}),
-                _tabItem('Emv', ColorHelper.emv, () {}),
-              ]
-          )
-        ],
-      ),
-    ),
-  );
-}
-
-_tabItem(String text, Color color, VoidCallback onPressed) {
-  return Expanded(
-      flex: 1,
-      child: Container(
-        height: 112,
-        margin: const EdgeInsets.fromLTRB(4, 4, 4, 4),
-        decoration: const BoxDecoration(
-            borderRadius: BorderRadius.all(
-                Radius.circular(8)
-            )
-        ),
-        child: MaterialButton(
-          onPressed: onPressed,
-          child: Text(text,
-              style: const TextStyle(color: Colors.white, fontSize: 18)
-          ),
-          color: color,
-        ),
-      )
-  );
 }
---
+++ lib/pages/HomeScreen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:sun_mi_flutter_sdk/theme/colors.dart';
+
+import 'BasicPage.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  const HomeScreen( { Key ? key } ) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('S U N M I Flutter Sdk'),
+        ),
+        body: _mainView(context),
+      ),
+    );
+  }
+
+  _mainView(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          children: [
+            Row(
+                children:[
+                  _tabItem('Basic', ColorHelper.basic, () { _gotoBasicPage(context); } ),
+                  _tabItem('Print', ColorHelper.print, () { _gotoBasicPage(context); } ),
+                ]
+            ),
+            Row(
+                children:[
+                  _tabItem('Scan', ColorHelper.security, () {}),
+                  _tabItem('Emv', ColorHelper.emv, () {}),
+                ]
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _gotoBasicPage(context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (context) =>
+        const BasicPage()
+      )
+    );
+  }
+
+  _tabItem(String text, Color color, VoidCallback onPressed) {
+    return Expanded(
+        flex: 1,
+        child: Container(
+          height: 112,
+          margin: const EdgeInsets.fromLTRB(4, 4, 4, 4),
+          decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(
+                  Radius.circular(8)
+              )
+          ),
+          child: MaterialButton(
+            onPressed: onPressed,
+            child: Text(text,
+                style: const TextStyle(color: Colors.white, fontSize: 18)
+            ),
+            color: color,
+          ),
+        )
+    );
+  }
+
+}
