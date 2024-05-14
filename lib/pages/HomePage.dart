d2ed2522d0e0124cd8681647918246cdb84e21dd
chuwuwang <chuwuwang@126.com>
2024-05-14 16:23:28 +0400

    feat: tidy code

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:sun_mi_flutter_sdk/pages/basic/BasicPage.dart';
 import 'package:sun_mi_flutter_sdk/pages/print/PrintPage.dart';
 import 'package:sun_mi_flutter_sdk/pages/scan/ScanPage.dart';
 import 'package:sun_mi_flutter_sdk/theme/colors.dart';
-
-import 'basic/BasicPage.dart';
 
 class HomePage extends StatelessWidget {
 
@@ -11,70 +10,55 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('S U N M I Flutter Sdk'), ),
-        body: _mainView(context),
-      ),
-    );
+    var text = const Text('S U N M I Flutter SDK');
+    var appBar = AppBar(title: text);
+    var body = _mainView(context);
+    var scaffold = Scaffold(appBar: appBar, body: body);
+    return MaterialApp(home: scaffold);
   }
 
   _mainView(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        padding: const EdgeInsets.all(8),
-        child: Column(
-          children: [
-            Row(
-                children:[
-                  _tabItem('Basic', ColorHelper.basic, () { _gotoBasicPage(context); } ),
-                  _tabItem('Print', ColorHelper.print, () { _gtoPrintPage(context); } ),
-                ]
-            ),
-            Row(
-                children:[
-                  _tabItem('Scan', ColorHelper.security, () { _gotoScanPage(context); } ),
-                  _tabItem('Emv', ColorHelper.emv, () { } ),
-                ]
-            )
-          ],
-        ),
-      ),
-    );
+    List<Widget> children1st = [
+      _tabItem('Basic', ColorHelper.basic, () { _gotoBasicPage(context); } ),
+      _tabItem('Print', ColorHelper.print, () { _gtoPrintPage(context); } ),
+    ];
+    List<Widget> children2nd = [
+      _tabItem('Scan', ColorHelper.scan, () { _gotoScanPage(context); } ),
+      _tabItem('Emv', ColorHelper.emv, () { _gtoPrintPage(context); } ),
+    ];
+    var row1st = Row(children: children1st);
+    var row2nd = Row(children: children2nd);
+    List<Widget> children = [row1st, row2nd];
+    var column = Column(children: children);
+    var container = Container(padding: const EdgeInsets.all(8), child: column);
+    return SingleChildScrollView(child: container);
   }
 
   _gotoBasicPage(context) {
-    Navigator.of(context).push(
-      MaterialPageRoute(builder: (context) => const BasicPage(), )
-    );
+    builder(context) => const BasicPage();
+    var route = MaterialPageRoute(builder: builder);
+    Navigator.of(context).push(route);
   }
 
   _gtoPrintPage(context) {
-    Navigator.of(context).push(
-        MaterialPageRoute(builder: (context) => const PrintPage(), )
-    );
+    builder(context) => const PrintPage();
+    var route = MaterialPageRoute(builder: builder);
+    Navigator.of(context).push(route);
   }
 
   _gotoScanPage(context) {
-    Navigator.of(context).push(
-        MaterialPageRoute(builder: (context) => const ScanPage(), )
-    );
+    builder(context) => const ScanPage();
+    var route = MaterialPageRoute(builder: builder);
+    Navigator.of(context).push(route);
   }
 
   _tabItem(String text, Color color, VoidCallback onPressed) {
-    return Expanded(
-        flex: 1,
-        child: Container(
-          height: 112,
-          margin: const EdgeInsets.fromLTRB(4, 4, 4, 4),
-          child: MaterialButton(
-            color: color,
-            onPressed: onPressed,
-            child: Text(text, style: const TextStyle(color: Colors.white, fontSize: 18), ),
-            shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(8), ),
-          ),
-        )
-    );
+    var borderRadius = BorderRadius.circular(8);
+    var roundedRectangleBorder = RoundedRectangleBorder(borderRadius: borderRadius);
+    var textStyle = const TextStyle(color: Colors.white, fontSize: 18);
+    var button = MaterialButton(color: color, onPressed: onPressed, child: Text(text, style: textStyle), shape: roundedRectangleBorder);
+    var container = Container(height: 112, margin: const EdgeInsets.fromLTRB(4, 4, 4, 4), child: button);
+    return Expanded(flex: 1, child: container);
   }
 
 }
--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
 class ColorHelper {
+
   static const Color emv = Color(0XFFFF6600);
   static const Color other = Color(0XFF607B8B);
   static const Color basic = Color(0XFF66CC33);
   static const Color print = Color(0XFF3399FF);
+  static const Color scan = Color(0XFFFFCC00);
   static const Color security = Color(0XFFFFCC00);
+
 }
